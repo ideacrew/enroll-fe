b6e05478a5a1a1e9b5ed4b0bfca533904269478f
Mark Goho <markgoho@gmail.com>
2021-05-17 11:06:26 -0400

    initial commit

---
+++ apps/coverme/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'enroll-front-end-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'coverme';
+}
