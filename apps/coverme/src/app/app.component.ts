48b75eb56fb986f0072dc162f41e3d96b8d18a8d
Mark Goho <markgoho@gmail.com>
2021-05-20 08:57:26 -0400

    update prefixes and project name

--- apps/coverme/src/app/app.component.ts
+++ apps/coverme/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'enroll-front-end-root',
+  selector: 'coverme-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
