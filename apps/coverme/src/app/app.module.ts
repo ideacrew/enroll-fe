ee6118ad8d1905e298c33e157eec990246fa402f
Mark Goho <markgoho@gmail.com>
2021-05-23 15:32:20 -0400

    add transloco

--- apps/coverme/src/app/app.module.ts
+++ apps/coverme/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TranslocoRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
