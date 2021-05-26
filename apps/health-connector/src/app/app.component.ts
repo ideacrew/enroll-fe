75fa8010e7944418a62dcad349b7e218f3c1ec23
Mark Goho <markgoho@gmail.com>
2021-05-25 22:31:26 -0400

    fix lint error

--- apps/health-connector/src/app/app.component.ts
+++ apps/health-connector/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'health-connector-root',
+  selector: 'mhc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'health-connector';
-}
+export class AppComponent {}
