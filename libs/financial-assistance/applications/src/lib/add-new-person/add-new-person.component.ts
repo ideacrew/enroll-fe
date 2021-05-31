e4d12138330db8c85b17d34eeda3aa98e39c2b2d
Mark Goho <markgoho@gmail.com>
2021-05-31 13:44:26 -0400

    working through add-new-person page

--- libs/financial-assistance/applications/src/lib/add-new-person/add-new-person.component.ts
+++ libs/financial-assistance/applications/src/lib/add-new-person/add-new-person.component.ts
@@ -5,4 +5,13 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./add-new-person.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddNewPersonComponent {}
+export class AddNewPersonComponent {
+  firstName!: string;
+  needsCoverage: 'yes' | 'no' = 'yes';
+  livesWithPrimary: 'yes' | 'no' = 'no';
+  citizenOrNational!: 'yes' | 'no';
+
+  get firstNameHasValue(): boolean {
+    return this.firstName?.length > 0;
+  }
+}
