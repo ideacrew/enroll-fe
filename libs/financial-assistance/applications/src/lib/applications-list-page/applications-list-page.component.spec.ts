2a7a71544928e81b3d13e9b6c9c5696e1519a6aa
Mark Goho <markgoho@gmail.com>
2021-06-07 17:24:43 -0400

    update spec dependency

--- libs/financial-assistance/applications/src/lib/applications-list-page/applications-list-page.component.spec.ts
+++ libs/financial-assistance/applications/src/lib/applications-list-page/applications-list-page.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ApplicationsListPageComponent } from './applications-list-page.component';
 
-describe('ApplicationsPageComponent', () => {
+describe('ApplicationsListPageComponent', () => {
   let component: ApplicationsListPageComponent;
   let fixture: ComponentFixture<ApplicationsListPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ApplicationsListPageComponent],
     }).compileComponents();
   });
