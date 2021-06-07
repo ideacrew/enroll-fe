95dadc1ad38a078a3cdc0b29df09800e1a3bbb7f
Mark Goho <markgoho@gmail.com>
2021-06-07 11:47:33 -0400

    move other questions to own module

--- libs/financial-assistance/applicant/src/lib/financial-assistance-applicant.module.ts
+++ libs/financial-assistance/applicant/src/lib/financial-assistance-applicant.module.ts
@@ -1,87 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ApplicantComponent } from './applicant/applicant.component';
-import { OtherQuestionsComponent } from './other-questions/other-questions.component';
-import { PregnantComponent } from './pregnant/pregnant.component';
-import { StudentComponent } from './student/student.component';
-import { BlindComponent } from './blind/blind.component';
-import { LongTermCareComponent } from './long-term-care/long-term-care.component';
-import { MedicalBillsComponent } from './medical-bills/medical-bills.component';
-import { DisabilityComponent } from './disability/disability.component';
-import { FosterCareComponent } from './foster-care/foster-care.component';
-import { TaxInfoModule } from './tax-info/tax-info.module';
-import { JobIncomeModule } from './job-income/job-income.module';
-import { OtherIncomeModule } from './other-income/other-income.module';
-import { DeductionsModule } from './deductions/deductions.module';
-import { BenefitsModule } from './benefits/benefits.module';
+import { ApplicantRoutingModule } from './applicant-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: ApplicantComponent,
-        children: [
-          { path: 'tax-info', loadChildren: () => TaxInfoModule },
-          { path: 'job-income', loadChildren: () => JobIncomeModule },
-          {
-            path: 'other-income',
-            loadChildren: () => OtherIncomeModule,
-          },
-
-          { path: 'income-adjustments', loadChildren: () => DeductionsModule },
-          { path: 'health-coverage', loadChildren: () => BenefitsModule },
-
-          {
-            path: 'other-questions/pregnant',
-            component: PregnantComponent,
-          },
-          {
-            path: 'other-questions/student',
-            component: StudentComponent,
-          },
-          {
-            path: 'other-questions/foster-care',
-            component: FosterCareComponent,
-          },
-          {
-            path: 'other-questions/blind',
-            component: BlindComponent,
-          },
-          {
-            path: 'other-questions/daily-assistance',
-            component: LongTermCareComponent,
-          },
-          {
-            path: 'other-questions/medical-bills',
-            component: MedicalBillsComponent,
-          },
-          {
-            path: 'other-questions/disability',
-            component: DisabilityComponent,
-          },
-          { path: 'other-questions', redirectTo: 'other-questions/pregnant' },
-        ],
-      },
-    ]),
-    BenefitsModule,
+    ApplicantRoutingModule,
   ],
-  declarations: [
-    ApplicantComponent,
-    OtherQuestionsComponent,
-    PregnantComponent,
-    StudentComponent,
-    BlindComponent,
-    LongTermCareComponent,
-    MedicalBillsComponent,
-    DisabilityComponent,
-    FosterCareComponent,
-  ],
+  declarations: [ApplicantComponent],
 })
 export class FinancialAssistanceApplicantModule {}
--- libs/financial-assistance/applicant/src/lib/other-questions/other-questions.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  templateUrl: './other-questions.component.html',
-  styleUrls: ['./other-questions.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class OtherQuestionsComponent {}
