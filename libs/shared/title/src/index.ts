8bdac52c68591680fc7ac1c111a265f3b6173cd9
Mark Goho <markgoho@gmail.com>
2021-06-17 19:14:25 -0400

    get rid of unnecessary files

--- libs/shared/title/src/index.ts
+++ libs/shared/title/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/shared-title.module';
 export * from './lib/title.service';
 export * from './lib/title-extension.token';
--- libs/util/helpers/src/index.ts
+++ libs/util/helpers/src/index.ts
@@ -1,2 +1,1 @@
-export * from './lib/util-helpers.module';
 export * from './lib/filterNullish.operator';
