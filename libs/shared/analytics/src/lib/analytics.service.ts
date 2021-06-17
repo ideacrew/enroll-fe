8959aca1b0ad06c8cdcee91ae2c9433a9638b32d
Mark Goho <markgoho@gmail.com>
2021-06-17 10:37:14 -0400

    add page tracking to analytics service

--- libs/shared/analytics/src/lib/analytics.service.ts
+++ libs/shared/analytics/src/lib/analytics.service.ts
@@ -1,16 +1,33 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable no-var */
 import { Injectable, Inject } from '@angular/core';
+import { Router, NavigationEnd, RouterEvent, Event } from '@angular/router';
+import { filter, tap } from 'rxjs/operators';
+
 import { removeAuthToken } from './removeAuthToken';
 import { TRACKING_ID } from './trackingId';
 
 declare var gtag: any;
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class AnalyticsService {
-  constructor(@Inject(TRACKING_ID) private trackingId: string) {}
+  constructor(
+    @Inject(TRACKING_ID) private trackingId: string,
+    private router: Router
+  ) {
+    this.router.events
+      .pipe(
+        filter<Event, RouterEvent>(
+          (event: Event): event is RouterEvent => event instanceof RouterEvent
+        ),
+        filter<RouterEvent, NavigationEnd>(
+          (event: RouterEvent): event is NavigationEnd =>
+            event instanceof NavigationEnd
+        ),
+        tap((event: NavigationEnd) => this.sendPage(event.urlAfterRedirects))
+      )
+      .subscribe();
+  }
 
   // Sets the userId for tracking
   // https://developers.google.com/analytics/devguides/collection/gtagjs/cookies-user-id#set_user_id
