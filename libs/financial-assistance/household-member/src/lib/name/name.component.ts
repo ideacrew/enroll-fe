6fcee490b39fe4bb63d2e1670a84cbb16f21f317
Mark Goho <markgoho@gmail.com>
2021-06-21 11:43:12 -0400

    remove boilerplate

--- libs/financial-assistance/household-member/src/lib/name/name.component.ts
+++ libs/financial-assistance/household-member/src/lib/name/name.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'enroll-name',
   templateUrl: './name.component.html',
   styleUrls: ['./name.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NameComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NameComponent {}
