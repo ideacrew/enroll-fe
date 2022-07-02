c8b6cbdd524fce29efb14fc6a86943968c5913ff
Mark Goho <markgoho@gmail.com>
2022-07-02 21:02:39 +0000

    add i18n lib

---
+++ libs/shared/i18n/src/lib/shared-i18n.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  translocoConfig,
+  TranslocoModule,
+  TRANSLOCO_CONFIG,
+  TRANSLOCO_LOADER,
+  TRANSLOCO_MISSING_HANDLER,
+} from '@ngneat/transloco';
+import { TranslocoHttpLoader } from './transloco.loader';
+import { CustomHandler } from './custom-handler';
+
+@NgModule({
+  imports: [CommonModule, HttpClientModule],
+  providers: [
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: translocoConfig({
+        availableLangs: ['en', 'es'],
+        defaultLang: 'en',
+        // Remove this option if your application
+        // doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        // prodMode: environment.production,
+        prodMode: false,
+      }),
+    },
+    { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader },
+    { provide: TRANSLOCO_MISSING_HANDLER, useClass: CustomHandler },
+  ],
+  exports: [TranslocoModule],
+})
+export class SharedI18nModule {}
