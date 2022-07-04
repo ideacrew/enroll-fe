e4c453bb653705c296580ae4b00c149eec58ad8e
Mark Goho <markgoho@gmail.com>
2022-07-04 15:10:09 +0000

    add coverme i18n lib

--- apps/coverme/src/app/app.module.ts
+++ apps/coverme/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CovermeI18nModule } from '@enroll/coverme/i18n';
+import { SharedI18nModule } from '@enroll/shared/i18n';
+import { RootStoreModule } from '@enroll/state/root-store';
+import { UiBaseShellModule } from '@enroll/ui/base/shell';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    SharedI18nModule,
+    CovermeI18nModule,
+    RootStoreModule,
+    UiBaseShellModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
