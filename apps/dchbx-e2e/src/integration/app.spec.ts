32fc522ec085fd26091beb50d324b0dd2c1b57f6
Mark Goho <markgoho@gmail.com>
2022-07-05 18:48:57 +0000

    add assets to project.json

--- apps/dchbx-e2e/src/integration/app.spec.ts
+++ apps/dchbx-e2e/src/integration/app.spec.ts
@@ -1,13 +1,11 @@
-import { getGreeting } from '../support/app.po';
-
 describe('dchbx', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome dchbx');
+    cy.get('h1').contains('Welcome to this State-based Marketplace');
   });
 });
