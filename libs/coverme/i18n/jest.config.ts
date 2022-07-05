7a7216849b1603c401fce0fa88bed7f011e689ea
Mark Goho <markgoho@gmail.com>
2022-07-05 18:31:47 +0000

    move jest config properties

--- libs/coverme/i18n/jest.config.ts
+++ libs/coverme/i18n/jest.config.ts
@@ -10,13 +10,13 @@ export default {
     },
   },
   coverageDirectory: '../../../coverage/libs/coverme/i18n',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
--- libs/financial-assistance/household-member/jest.config.ts
+++ libs/financial-assistance/household-member/jest.config.ts
@@ -11,13 +11,13 @@ export default {
   },
   coverageDirectory:
     '../../../coverage/libs/financial-assistance/household-member',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
