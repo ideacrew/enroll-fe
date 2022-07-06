43928d3f0b93f1759869959f9ac714d7e8686538
Mark Goho <markgoho@gmail.com>
2022-07-06 19:30:05 +0000

    add base styles and assets to carrier portal

--- apps/carrier-portal/src/app/app.module.ts
+++ apps/carrier-portal/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { RootStoreModule } from '@enroll/shared/state/root-store';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot([]), RootStoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
