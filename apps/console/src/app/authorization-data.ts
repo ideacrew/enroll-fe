fd970ed4f9c149edce98ff5e160dc0c6dd74c3d2
Trey Evans <lewis.r.evans@gmail.com>
2022-07-26 16:56:42 -0400

    Added authorization data interfaces.

---
+++ apps/console/src/app/authorization-data.ts
@@ -0,0 +1,22 @@
+// Returned from GET /realms
+export interface RealmListResponse {
+  realms: Array<string>;
+}
+
+// Submitted to POST /sessions
+export interface LoginRequest {
+  username: string;
+  password: string;
+  realm_name: string;
+}
+
+// Returned from POST /sessions, and from POST /sessions/refresh
+export interface TokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
+// Submitted to POST /sessions/refresh
+export interface RefreshTokenRequest {
+  refresh_token: string;
+}
