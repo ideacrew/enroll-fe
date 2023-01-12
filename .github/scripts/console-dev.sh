322d3d34642fada23154d41fe7a7fe8090f5de0b
Mark Goho <markgoho@gmail.com>
2023-01-12 14:33:21 +0000

    update deployment to use ic and me credentials

---
+++ .github/scripts/console-dev.sh
@@ -0,0 +1,4 @@
+cd dist/apps/console
+aws s3 sync . s3://cme-console-preprod/ --delete
+invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E5RER2GERTJUC --output text --query 'Invalidation.Id' --paths '/*'`
+aws cloudfront wait invalidation-completed --distribution-id E5RER2GERTJUC --id ${invalidationId}
