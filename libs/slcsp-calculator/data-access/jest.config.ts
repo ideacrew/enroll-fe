22ad5ff25e84352958e7f0337d349a486364f82e
Mark Goho <markgoho@gmail.com>
2023-01-12 13:47:31 +0000

    remove abusive eslint rule

--- libs/slcsp-calculator/data-access/jest.config.ts
+++ libs/slcsp-calculator/data-access/jest.config.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/naming-convention */
 export default {
   displayName: 'slcsp-calculator-data-access',
   preset: '../../../jest.preset.js',
