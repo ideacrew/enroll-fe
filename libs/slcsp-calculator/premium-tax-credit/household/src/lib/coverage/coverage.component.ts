692010382e4452971a96268e147992b081bcb913
Mark Goho <markgoho@gmail.com>
2023-01-22 02:21:12 +0000

    add dynamic navigation on coverage page

--- libs/slcsp-calculator/premium-tax-credit/household/src/lib/coverage/coverage.component.ts
+++ libs/slcsp-calculator/premium-tax-credit/household/src/lib/coverage/coverage.component.ts
@@ -4,6 +4,7 @@ import { map, shareReplay, tap } from 'rxjs';
 
 import { selectRouteParam } from '@enroll/shared/state/root-store';
 import { HouseholdService } from '@enroll/slcsp-calculator/household-form';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './coverage.component.html',
@@ -15,6 +16,7 @@ export class CoverageComponent {
   householdFormGroup = this.householdService.householdForm;
   householdMembersArray = this.householdService.householdMembersArray;
   memberId!: number;
+  router = inject(Router);
   store = inject(Store);
   memberId$ = this.store.select(selectRouteParam('memberId')).pipe(
     map((memberId) => Number.parseInt(memberId ?? '1', 10)),
@@ -26,4 +28,22 @@ export class CoverageComponent {
     map((memberId) => this.householdMembersArray.at(memberId - 1)?.value),
     map((householdMember) => householdMember.name)
   );
+
+  nextStep() {
+    if (this.memberId > this.householdMembersArray.length) {
+      throw new Error('Invalid member id');
+    }
+
+    // need to navigate to either the next member of the household
+    // or the review page if this is the last member of the household
+    if (this.memberId < this.householdMembersArray.length) {
+      void this.router.navigateByUrl(
+        `/premium-tax-credit/household/member/${this.memberId + 1}`
+      );
+    }
+
+    if (this.memberId === this.householdMembersArray.length) {
+      void this.router.navigateByUrl(`/premium-tax-credit/review`);
+    }
+  }
 }
