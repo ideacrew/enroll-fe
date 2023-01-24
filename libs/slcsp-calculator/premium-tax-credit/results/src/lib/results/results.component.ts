8e63cb76e8e0a6d9a0e965d321edd9794845aedd
Mark Goho <markgoho@gmail.com>
2023-01-24 01:26:08 +0000

    style results page

--- libs/slcsp-calculator/premium-tax-credit/results/src/lib/results/results.component.ts
+++ libs/slcsp-calculator/premium-tax-credit/results/src/lib/results/results.component.ts
@@ -5,7 +5,10 @@ import {
   OnInit,
 } from '@angular/core';
 
-import { HouseholdService } from '@enroll/slcsp-calculator/household-form';
+import {
+  HouseholdService,
+  months,
+} from '@enroll/slcsp-calculator/household-form';
 
 @Component({
   templateUrl: './results.component.html',
@@ -14,8 +17,28 @@ import { HouseholdService } from '@enroll/slcsp-calculator/household-form';
 })
 export class ResultsComponent implements OnInit {
   householdService = inject(HouseholdService);
+  monthList = months;
+
+  slcspPremiums = {
+    jan: 965.35,
+    feb: 965.35,
+    mar: 965.35,
+    apr: 965.35,
+    may: 965.35,
+    jun: 965.35,
+    jul: 965.35,
+    aug: 965.35,
+    sep: 965.35,
+    oct: 965.35,
+    nov: 965.35,
+    dec: 965.35,
+  };
 
   ngOnInit(): void {
     console.log(this.householdService.householdForm.value);
+  }
+
+  print(): void {
+    window.print();
   }
 }
