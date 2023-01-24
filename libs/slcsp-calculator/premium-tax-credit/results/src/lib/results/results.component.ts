6810553ffd65ef0e8d13d2b7ea7d79ff3ed3df44
Mark Goho <markgoho@gmail.com>
2023-01-24 00:42:32 +0000

    log form on results page

--- libs/slcsp-calculator/premium-tax-credit/results/src/lib/results/results.component.ts
+++ libs/slcsp-calculator/premium-tax-credit/results/src/lib/results/results.component.ts
@@ -1,8 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+
+import { HouseholdService } from '@enroll/slcsp-calculator/household-form';
 
 @Component({
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ResultsComponent {}
+export class ResultsComponent implements OnInit {
+  householdService = inject(HouseholdService);
+
+  ngOnInit(): void {
+    console.log(this.householdService.householdForm.value);
+  }
+}
