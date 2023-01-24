a9563962b4b17486f6cfa4295f31d8107451e0a5
Mark Goho <markgoho@gmail.com>
2023-01-23 22:29:46 -0500

    fix console e2e spec, turn video off

--- apps/console-e2e/cypress.config.ts
+++ apps/console-e2e/cypress.config.ts
@@ -5,12 +5,6 @@ export default defineConfig({
   e2e: {
     // eslint-disable-next-line unicorn/prefer-module
     ...nxE2EPreset(__dirname),
-    /**
-     * TODO(@nrwl/cypress): In Cypress v12,the testIsolation option is turned on by default.
-     * This can cause tests to start breaking where not intended.
-     * You should consider enabling this once you verify tests do not depend on each other
-     * More Info: https://docs.cypress.io/guides/references/migration-guide#Test-Isolation
-     **/
-    testIsolation: false,
+    video: false,
   },
 });
--- apps/slcsp-calculator-e2e/cypress.config.ts
+++ apps/slcsp-calculator-e2e/cypress.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  viewportWidth: 1400,
-  viewportHeight: 800,
   e2e: {
     // eslint-disable-next-line unicorn/prefer-module
     ...nxE2EPreset(__dirname),
+    viewportWidth: 1400,
+    viewportHeight: 800,
+    video: false,
   },
 });
