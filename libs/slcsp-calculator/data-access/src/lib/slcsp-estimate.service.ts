9a26b05bf6a76a1e81e4b3e052cc66bd34ab4dad
Mark Goho <markgoho@gmail.com>
2023-01-26 15:44:38 -0500

    update estimate service

--- libs/slcsp-calculator/data-access/src/lib/slcsp-estimate.service.ts
+++ libs/slcsp-calculator/data-access/src/lib/slcsp-estimate.service.ts
@@ -1,41 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-
-import {
-  APPLICATION_NAME,
-  getBaseUrl,
-  TenantConfigService,
-} from '@enroll/tenant-config';
 import { Observable } from 'rxjs';
+
+import { TenantConfigService } from '@enroll/tenant-config';
+
 import { SlcspEstimate } from './interfaces/slcsp-estimate';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SlcspEstimateService {
-  private readonly tenant = inject(TenantConfigService).tenant;
-  private readonly hostName = window.location.host;
-  private readonly applicationName = inject(APPLICATION_NAME);
+  private readonly tenantConfig = inject(TenantConfigService).tenantConfig;
   private readonly http = inject(HttpClient);
-
-  readonly baseUrl = getBaseUrl({
-    host: this.hostName,
-    tenant: this.tenant,
-    application: this.applicationName,
-  });
-
-  constructor() {
-    console.log({
-      hostName: this.hostName,
-      tenant: this.tenant,
-      baseUrl: this.baseUrl,
-      application: this.applicationName,
-    });
-  }
 
   getSlcspEstimate(formValue: unknown): Observable<SlcspEstimate> {
     return this.http.post<SlcspEstimate>(
-      `${this.baseUrl}/api/v2/slcsp_calculator/estimate`,
+      `${this.tenantConfig.baseApiUrl}/api/v2/slcsp_calculator/estimate`,
       formValue
     );
   }
