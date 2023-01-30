635fa16674a22e6eb043586346fff89db13e0c33
Mark Goho <markgoho@gmail.com>
2023-01-30 19:13:47 +0000

    update favicon script to exit if no affected apps

--- .github/scripts/update-favicon.js
+++ .github/scripts/update-favicon.js
@@ -7,6 +7,12 @@ if (!tenant) {
   throw new Error('TENANT environment variable not set');
 }
 
+// Check if the dist/apps directory exists, exit if it does not
+if (!fs.existsSync('dist/apps')) {
+  console.log('Dist directory does not exist, exiting script');
+  return;
+}
+
 // Grab reference to the correct favicon file
 const faviconPath = `dist/apps/slcsp-calculator/assets/logos/${tenant}-favicon.svg`;
 
