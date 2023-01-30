ec7e0f0a515dfdaee9da7c3090d21c138f548ed3
Mark Goho <markgoho@gmail.com>
2023-01-30 17:09:38 +0000

    set max attempts for aws in deploy scripts

--- .github/scripts/console-dev.sh
+++ .github/scripts/console-dev.sh
@@ -1,3 +1,5 @@
+export AWS_MAX_ATTEMPTS=10
+
 cd dist/apps/console
 aws s3 sync . s3://cme-console-preprod/ --delete
 invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E5RER2GERTJUC --output text --query 'Invalidation.Id' --paths '/*'`
--- .github/scripts/slcsp-prod.sh
+++ .github/scripts/slcsp-prod.sh
@@ -1,3 +1,5 @@
+export AWS_MAX_ATTEMPTS=10
+
 cd dist/apps/slcsp-calculator
 aws s3 sync --acl public-read . s3://slcsp-calculator-cme/ --delete
 invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E3DAFP9MLBBRM9 --output text --query 'Invalidation.Id' --paths '/*'`
