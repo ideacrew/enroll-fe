8bf3a5422f95b22d891d2fe4f438cd18578aef89
Mark Goho <markgoho@gmail.com>
2023-01-31 18:33:00 -0500

    update script for prod release

--- .github/scripts/slcsp-dev.sh
+++ .github/scripts/slcsp-dev.sh
@@ -1,5 +1,3 @@
-export AWS_MAX_ATTEMPTS=10
-
 cd dist/apps/slcsp-calculator
 aws s3 sync --acl public-read . s3://dev-slcsp-calculator-cme/ --delete
 invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E25P6ELTA2CD2H --output text --query 'Invalidation.Id' --paths '/*'`
--- .github/scripts/slcsp-prod.sh
+++ .github/scripts/slcsp-prod.sh
@@ -1,6 +1,4 @@
-export AWS_MAX_ATTEMPTS=10
-
 cd dist/apps/slcsp-calculator
-aws s3 sync --acl public-read . s3://slcsp-calculator-cme/ --delete
-invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E3DAFP9MLBBRM9 --output text --query 'Invalidation.Id' --paths '/*'`
-aws cloudfront wait invalidation-completed --distribution-id E3DAFP9MLBBRM9 --id ${invalidationId}
+aws s3 sync --acl public-read . s3://cme-slcsp-calculator-prod/ --delete
+invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E1HFKIGE662X1V --output text --query 'Invalidation.Id' --paths '/*'`
+aws cloudfront wait invalidation-completed --distribution-id E1HFKIGE662X1V --id ${invalidationId}
