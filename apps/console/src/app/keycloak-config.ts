173cd8ded299e7a78491f4f490e20650d57ef3a0
Trey Evans <lewis.r.evans@gmail.com>
2023-05-10 17:56:53 -0400

    Trigger build.

--- apps/console/src/app/keycloak-config.ts
+++ apps/console/src/app/keycloak-config.ts
@@ -4,13 +4,19 @@ export const keycloakConfiguration: KeycloakConfig[] = [
   {
     host: 'localhost',
     clientId: 'openid',
-    url: 'http://localhost:8080/auth',
     realm: 'Anthem',
+    url: 'http://localhost:8080/auth',
   },
   {
     host: 'preprod-console.cme.openhbx.org',
     clientId: 'openid',
     realm: 'preprod',
     url: 'https://preprod-sso.priv.cme.openhbx.org/auth',
+  },
+  {
+    host: 'carrier-portal.coverme.gov',
+    clientId: 'openid',
+    realm: 'production',
+    url: 'TBD',
   },
 ];
