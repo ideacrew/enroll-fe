25a803cbc915a84881e3f01971544a4878a328fe
Trey Evans <lewis.r.evans@gmail.com>
2023-05-15 19:01:37 -0400

    Correct multiple UI elements.

--- libs/carrier-portal/ui/src/lib/pipes/index.ts
+++ libs/carrier-portal/ui/src/lib/pipes/index.ts
@@ -2,6 +2,7 @@ export * from './format-ssn.pipe';
 export * from './parse-edi-data.pipe';
 export * from './parse-raw-date.pipe';
 export * from './parse-raw-time.pipe';
+export * from './phone-number.pipe';
 export * from './sort-by-date.pipe';
 export * from './sort-by-status.pipe';
 export * from './sort-by-policy-start.pipe';
