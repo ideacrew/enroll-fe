47f183157494e7523bd570ab3274da587963f67c
Trey Evans <lewis.r.evans@gmail.com>
2023-05-23 23:53:38 -0400

    Correct UI errors for phone and labeling.

--- libs/carrier-portal/ui/src/lib/pipes/phone-number.pipe.ts
+++ libs/carrier-portal/ui/src/lib/pipes/phone-number.pipe.ts
@@ -11,21 +11,21 @@ export class PhoneNumberPipe implements PipeTransform {
 
   private formatPhoneNumber(number: string) {
     if (number.length === 7) {
-      const top = number.slice(0, 2);
-      const rest = number.slice(3, 6);
+      const top = number.slice(0, 3);
+      const rest = number.slice(3, 7);
       return `${top}-${rest}`;
     }
     if (number.length === 10) {
-      const ac = number.slice(0, 2);
-      const top = number.slice(3, 5);
-      const rest = number.slice(6, 9);
+      const ac = number.slice(0, 3);
+      const top = number.slice(3, 6);
+      const rest = number.slice(6, 10);
       return `(${ac}) ${top}-${rest}`;
     }
     if (number.length === 11) {
       const first = number.slice(0, 1);
-      const ac = number.slice(1, 3);
-      const top = number.slice(4, 6);
-      const rest = number.slice(7, 10);
+      const ac = number.slice(1, 4);
+      const top = number.slice(4, 7);
+      const rest = number.slice(7, 11);
       return `${first} (${ac}) ${top}-${rest}`;
     }
     return number;
