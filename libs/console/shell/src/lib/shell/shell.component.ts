0be73fe2ed2e1590a7cf497268c5d26dc7184987
Trey Evans <lewis.r.evans@gmail.com>
2023-05-25 19:44:25 -0400

    Add required tests and translations.

--- libs/console/shell/src/lib/shell/shell.component.ts
+++ libs/console/shell/src/lib/shell/shell.component.ts
@@ -4,11 +4,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { KeycloakService } from 'keycloak-angular';
 import { PortalDataService } from '@enroll/carrier-portal/data-access';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'enroll-shell',
   standalone: true,
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterModule, HttpClientModule, TranslocoModule],
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
