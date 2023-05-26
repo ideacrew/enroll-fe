d79831652e5f8ea023f53adaed170f922ca75c7d
Trey <lewis.r.evans@gmail.com>
2023-05-26 11:12:42 -0400

    Correct UI errors for phone and labeling. (#40)
    * Correct UI errors for phone and labeling.
    
    * Add required tests and translations.

---
+++ libs/carrier-portal/ui/src/lib/pipes/phone-number.pipe.spec.ts
@@ -0,0 +1,22 @@
+import { of } from 'rxjs';
+import { PhoneNumberPipe } from './phone-number.pipe';
+
+describe('PhoneNumberPipe', () => {
+  let pipe: PhoneNumberPipe;
+
+  beforeEach(() => {
+    pipe = new PhoneNumberPipe();
+  });
+
+  it('handles a 7 number long phone number', () => {
+    expect(pipe.transform('1234567')).toBe('123-4567');
+  });
+
+  it('handles a 10 number long phone number', () => {
+    expect(pipe.transform('1234567890')).toBe('(123) 456-7890');
+  });
+
+  it('handles an 11 number long phone number', () => {
+    expect(pipe.transform('12345678901')).toBe('1 (234) 567-8901');
+  });
+});
