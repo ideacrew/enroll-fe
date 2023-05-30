a546f0e115e5698337cd5e3c7f046807291ecaf5
Matt Williams <m.a.dubya@gmail.com>
2023-05-30 15:03:21 -0400

    align environment variables

--- .github/scripts/console-prod.sh
+++ .github/scripts/console-prod.sh
@@ -2,5 +2,5 @@ export AWS_MAX_ATTEMPTS=10
 
 cd dist/apps/console
 aws s3 sync . s3://$CONSOLE_BUCKET_PROD/ --delete
-invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id $CONSOLE_DISTRIBUTION_ID --output text --query 'Invalidation.Id' --paths '/*'`
-aws cloudfront wait invalidation-completed --distribution-id $CONSOLE_DISTRIBUTION_ID --id ${invalidationId}
+invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id $CONSOLE_DISTRIBUTION_ID_PROD --output text --query 'Invalidation.Id' --paths '/*'`
+aws cloudfront wait invalidation-completed --distribution-id $CONSOLE_DISTRIBUTION_ID_PROD --id ${invalidationId}
