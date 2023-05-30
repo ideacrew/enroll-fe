8cea56ff394cdeb6184359ae47475e941609ce76
Matt Williams <m.a.dubya@gmail.com>
2023-05-30 14:48:21 -0400

    deploy console assets to prod

---
+++ .github/scripts/console-prod.sh
@@ -0,0 +1,6 @@
+export AWS_MAX_ATTEMPTS=10
+
+cd dist/apps/console
+aws s3 sync . s3://$CONSOLE_BUCKET_PROD/ --delete
+invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id $CONSOLE_DISTRIBUTION_ID --output text --query 'Invalidation.Id' --paths '/*'`
+aws cloudfront wait invalidation-completed --distribution-id $CONSOLE_DISTRIBUTION_ID --id ${invalidationId}
