9a7f6fdf2e1358113f67cd2b33a300c090512bb3
Brad Bodine <bbodine1@gmail.com>
2023-06-22 15:33:48 -0500

    remove lint guard-for-in and  overrides

--- libs/slcsp-calculator/testing/src/lib/form-entry/residence-months.ts
+++ libs/slcsp-calculator/testing/src/lib/form-entry/residence-months.ts
@@ -4,7 +4,6 @@ export const fillInResidenceMonths = (
   index: number,
   months: Record<Month, boolean>
 ) => {
-  // eslint-disable-next-line guard-for-in
   for (const month in months) {
     // Is month true?
     const isMonthTrue = months[month as Month];
