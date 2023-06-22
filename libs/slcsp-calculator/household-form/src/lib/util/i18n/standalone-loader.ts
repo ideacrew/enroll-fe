424156c815a5e70a134a8f7aae150c50732bd778
Brad Bodine <bbodine1@gmail.com>
2023-06-22 14:01:43 -0500

    remove overrides for unicorn no-array-reduce

--- libs/slcsp-calculator/household-form/src/lib/util/i18n/standalone-loader.ts
+++ libs/slcsp-calculator/household-form/src/lib/util/i18n/standalone-loader.ts
@@ -5,7 +5,6 @@ const availableLangs = ['en', 'es'];
 type Importer = (lang: string, root: string) => Promise<Translation>;
 
 export const scopeLoader = (importer: Importer, root = 'i18n') =>
-  // eslint-disable-next-line unicorn/no-array-reduce
   availableLangs.reduce((accumulator, language) => {
     accumulator[language] = () => importer(language, root);
     return accumulator;
