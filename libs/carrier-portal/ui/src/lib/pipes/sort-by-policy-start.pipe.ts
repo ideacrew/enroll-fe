ba53bcd1b4a7838d9f24856dabccd13f1240e857
Brad <bbodine1@gmail.com>
2023-09-29 11:39:55 -0500

    Update policy sort

--- libs/carrier-portal/ui/src/lib/pipes/sort-by-policy-start.pipe.ts
+++ libs/carrier-portal/ui/src/lib/pipes/sort-by-policy-start.pipe.ts
@@ -11,23 +11,41 @@ export class SortByPolicyStartPipe implements PipeTransform {
     const rawPolicies = [...policies];
 
     const sortedPolicies = rawPolicies.sort((a, b) => {
-      const firstDate = this.policyStartDate(a);
-      const secondDate = this.policyStartDate(b);
-      if (firstDate > secondDate) {
+      // Sort by plan year in descending order
+      const aPlanYear = new Date(a.plan.coverage_year).getFullYear();
+      const bPlanYear = new Date(b.plan.coverage_year).getFullYear();
+      if (aPlanYear > bPlanYear) {
         return -1;
-      } else if (firstDate < secondDate) {
+      } else if (aPlanYear < bPlanYear) {
         return 1;
       }
+
+      // Sort by effective date in descending order
+      const aEffectiveDate = new Date(a.enrollees[0].coverage_start);
+      const bEffectiveDate = new Date(b.enrollees[0].coverage_start);
+      if (aEffectiveDate > bEffectiveDate) {
+        return -1;
+      } else if (aEffectiveDate < bEffectiveDate) {
+        return 1;
+      }
+
+      // Sort by policy status (submitted/resubmitted policies first)
+      if (a.status === 'Submitted' || a.status === 'Resubmitted') {
+        return -1;
+      } else if (b.status === 'Submitted' || b.status === 'Resubmitted') {
+        return 1;
+      }
+
       return 0;
     });
+
     return sortedPolicies;
   }
-
-  private policyStartDate(policy: Policy): string {
-    const enrollees = policy.enrollees;
-    return enrollees
-      .map((a) => a.coverage_start)
-      .sort()
-      .reverse()[0];
-  }
+  // private policyStartDate(policy: Policy): string {
+  //   const enrollees = policy.enrollees;
+  //   return enrollees
+  //     .map((a) => a.coverage_start)
+  //     .sort()
+  //     .reverse()[0];
+  // }
 }
