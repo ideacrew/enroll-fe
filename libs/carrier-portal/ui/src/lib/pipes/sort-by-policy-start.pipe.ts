38302a5349af610baf9ba55306dea7251e0cdb36
Brad Bodine <bbodine1@gmail.com>
2023-10-04 13:30:40 -0500

    performance updates to sortbypolicystart pipe

--- libs/carrier-portal/ui/src/lib/pipes/sort-by-policy-start.pipe.ts
+++ libs/carrier-portal/ui/src/lib/pipes/sort-by-policy-start.pipe.ts
@@ -9,14 +9,15 @@ export class SortByPolicyStartPipe implements PipeTransform {
   transform(policies: Policy[]): Policy[] {
     return [...policies].sort((a, b) => {
       // First sort by coverage year derived from plan.coverage_year
-      if (policyStartDate(a) !== policyStartDate(b)) {
-        return policyStartDate(a) <= policyStartDate(b) ? 1 : -1;
+      const startDateA = policyStartDate(a);
+      const startDateB = policyStartDate(b);
+      if (startDateA !== startDateB) {
+        return startDateA <= startDateB ? 1 : -1;
       }
+
       // If they are the same coverage year, sort by status
-      if (policyStartDate(a) === policyStartDate(b)) {
-        if (a.status === 'Submitted') {
-          return a.status <= b.status ? 1 : -1;
-        }
+      if (a.status === 'Submitted') {
+        return a.status <= b.status ? 1 : -1;
       }
 
       return 0; // add default return value
