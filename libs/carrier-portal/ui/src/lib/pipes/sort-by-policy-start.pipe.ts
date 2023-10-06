b38e3c7301bba2c2c014e438c158fa504b8b0fcb
Trey Evans <lewis.r.evans@gmail.com>
2023-10-06 14:21:46 -0400

    Implement complex sorting logic.

--- libs/carrier-portal/ui/src/lib/pipes/sort-by-policy-start.pipe.ts
+++ libs/carrier-portal/ui/src/lib/pipes/sort-by-policy-start.pipe.ts
@@ -1,36 +1,98 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Pipe, PipeTransform } from '@angular/core';
-import { Policy } from '@enroll/carrier-portal/types';
+import { DateString } from '@enroll/carrier-portal/types';
+
+export type SortableEnrollee = {
+  coverage_start: DateString;
+  coverage_end?: DateString;
+  hbx_member_id: string;
+};
+
+export type SortablePolicy = {
+  subscriber_hbx_member_id: string;
+  enrollees: SortableEnrollee[];
+  status: string;
+};
 
 @Pipe({
   name: 'sortByPolicyStart',
   standalone: true,
 })
 export class SortByPolicyStartPipe implements PipeTransform {
-  transform(policies: Policy[]): Policy[] {
+  transform<T extends SortablePolicy>(policies: T[]): T[] {
     return [...policies].sort((a, b) => {
-      // First sort by coverage year derived from plan.coverage_year
-      const startDateA = policyStartDate(a);
-      const startDateB = policyStartDate(b);
+      const startDateA = this.policyStartDate(a);
+      const startDateB = this.policyStartDate(b);
+
+      // If they start on different dates, just sort by coverage start
       if (startDateA !== startDateB) {
         return startDateA <= startDateB ? 1 : -1;
       }
 
-      // If they are the same coverage year, sort by status
-      if (startDateA === startDateB) {
-        if (a.status === 'Submitted') {
-          return a.status <= b.status ? 1 : -1;
-        }
+      // If only one of them is canceled, it comes last
+      if (this.isCanceled(a) && !this.isCanceled(b)) {
+        return 1;
+      } else if (!this.isCanceled(a) && this.isCanceled(b)) {
+        return -1;
       }
 
-      return 0; // add default return value
+      // If only one of them is terminated, it comes last
+      if (this.isTerminated(a) && !this.isTerminated(b)) {
+        return 1;
+      } else if (!this.isTerminated(a) && this.isTerminated(b)) {
+        return -1;
+      }
+
+      // If the statuses are the same, do nothing
+      if (a.status === b.status) {
+        return 0;
+      }
+
+      // Otherwise, prefer based on certain statuses like 'submitted'
+      if (this.isSubmitted(a)) {
+        return -1;
+      } else if (this.isSubmitted(b)) {
+        return 1;
+      }
+      return 0;
     });
   }
-}
 
-function policyStartDate(policy: Policy): string {
-  const enrollees = policy.enrollees;
-  return enrollees
-    .map((a) => a.coverage_start)
-    .sort()
-    .reverse()[0];
+  isSubmitted(policy: SortablePolicy): boolean {
+    return policy.status.toLowerCase() === 'submitted';
+  }
+
+  isTerminated(policy: SortablePolicy): boolean {
+    const subscriber = policy.enrollees.find(
+      (a) => a.hbx_member_id === policy.subscriber_hbx_member_id
+    );
+    if (subscriber) {
+      const cEnd = subscriber.coverage_end;
+      if (cEnd) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isCanceled(policy: SortablePolicy): boolean {
+    const subscriber = policy.enrollees.find(
+      (a) => a.hbx_member_id === policy.subscriber_hbx_member_id
+    );
+    if (subscriber) {
+      const cEnd = subscriber.coverage_end;
+      if (cEnd) {
+        return cEnd <= subscriber.coverage_start;
+      }
+    }
+    return false;
+  }
+
+  policyStartDate(policy: SortablePolicy): string {
+    const enrollees = policy.enrollees;
+    return enrollees
+      .map((a) => a.coverage_start)
+      .sort()
+      .reverse()[0];
+  }
 }
